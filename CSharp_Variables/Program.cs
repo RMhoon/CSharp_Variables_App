d89b67a54637567100f2353afe1ce7c41694b630
RMhoon <r_mhoon@hotmail.com>
2021-02-19 20:16:58 -0500

    Initialize project

---
+++ CSharp_Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aStringExample = "";
+            int x = 10;
+            double y = 10.1;
+            char aSinggleCharacter = 'A';
+            bool isFinish = false;
+
+            Console.WriteLine($"string {aStringExample}; int {x} bool {isFinish} double {y}  char {aSinggleCharacter} ");
+
+            string dogName = "Ralph";
+            int dogAge = 10;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+        }
+    }
+}
